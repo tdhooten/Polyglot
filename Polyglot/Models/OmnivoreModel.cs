020ffec5b2dc436df9db358b10686a07ed64b44c
tdhooten <github.ejszn@simplelaogin.com>
2024-08-21 08:17:44 -0400

    Update Omnivore export formatting.

--- Polyglot/Models/OmnivoreModel.cs
+++ Polyglot/Models/OmnivoreModel.cs
@@ -2,9 +2,9 @@
 
 public class OmnivoreModel
 {
-    public string labels { get; set; } = String.Empty;
-    public string published_at { get; set; } = String.Empty;
-    public string saved_at { get; set; } = String.Empty;
-    public string state { get; set; } = String.Empty;
     public string url { get; set; } = String.Empty;
+    public string state { get; set; } = "SUCCEEDED";
+    public string labels { get; set; } = String.Empty;
+    public string saved_at { get; set; } = String.Empty;
+    public string published_at { get; set; } = String.Empty;
 }
